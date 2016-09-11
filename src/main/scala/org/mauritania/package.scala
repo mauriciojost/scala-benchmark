449cdbe5ecd7f20e0fbdc1bb99f3472d6ba261f3
Mauricio Jost <mauricio.jost@gmail.com>
2016-09-11 11:42:32 +0200

    Increase size of benchmark collections

--- src/main/scala/org/mauritania/package.scala
+++ src/main/scala/org/mauritania/package.scala
@@ -2,8 +2,8 @@ package org.mauritania
 
 package object mauritania {
   val seed = 1000
-  val aList = List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-  val anArray = Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-  val aVector = Vector(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-  val aSeq = Seq(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+  val aList = List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+  val anArray = Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+  val aVector = Vector(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+  val aSeq = Seq(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
 }
