c57a44b40892df0c28a366cc2418b458e99ffb78
Mauricio Jost <mauricio.jost@gmail.com>
2017-04-23 00:34:49 +0200

    Add benchmark on view...force

---
+++ src/main/scala/org/mauritania/minibenchmark/catalog/CollectionIterationIntermediate.scala
@@ -0,0 +1,18 @@
+package org.mauritania.minibenchmark.catalog
+
+import org.mauritania.mauritania._
+import org.openjdk.jmh.annotations.Benchmark
+
+class CollectionIterationIntermediate {
+
+  @Benchmark
+  def withIntermediateCollections(): Int = {
+    LargeList.map(i => i + 1).map(i => i * 2).sum
+  }
+
+  @Benchmark
+  def withoutIntermediateCollections(): Int = {
+    LargeList.view.map(i => i + 1).map(i => i * 2).force.sum
+  }
+
+}
